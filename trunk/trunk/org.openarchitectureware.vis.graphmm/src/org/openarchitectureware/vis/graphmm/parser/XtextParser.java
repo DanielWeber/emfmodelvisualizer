dd8275d7802f8c88f9ab3b972f43f354817b8abf
Markus Voelter <voelter@no.mail>
2008-11-22 11:43:41 +0000

    git-svn-id: http://emfmodelvisualizer.googlecode.com/svn@6 d18ea948-b79e-11dd-b040-2d45f096adfe

---
+++ trunk/trunk/org.openarchitectureware.vis.graphmm/src/org/openarchitectureware/vis/graphmm/parser/XtextParser.java
@@ -0,0 +1,12 @@
+
+package org.openarchitectureware.vis.graphmm.parser;
+
+import java.io.InputStream;
+
+public class XtextParser extends GenParser {
+
+	public XtextParser(InputStream in) {
+		super(in);
+	}
+
+}
