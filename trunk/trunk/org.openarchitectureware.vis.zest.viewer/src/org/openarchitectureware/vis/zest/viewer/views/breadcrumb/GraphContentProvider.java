f06ba30103db88d00ff93b351017657bb6cfc551
Philipp Bollbach <philipp.bollbach.dev@no.mail>
2009-04-14 09:38:53 +0000

    3: Build a breadcrumb feature http://code.google.com/p/emfmodelvisualizer/issues/detail?id=3
    changed the behaviour of the breadcrumb in the way that subgraphs of containernodes are no longer visible.
    from: graph>containernode>subgraph>node1
    to:  graph>containernode>node1
    
    git-svn-id: http://emfmodelvisualizer.googlecode.com/svn@88 d18ea948-b79e-11dd-b040-2d45f096adfe

--- trunk/trunk/org.openarchitectureware.vis.zest.viewer/src/org/openarchitectureware/vis/zest/viewer/views/breadcrumb/GraphContentProvider.java
+++ trunk/trunk/org.openarchitectureware.vis.zest.viewer/src/org/openarchitectureware/vis/zest/viewer/views/breadcrumb/GraphContentProvider.java
@@ -1,16 +1,8 @@
 package org.openarchitectureware.vis.zest.viewer.views.breadcrumb;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.zest.core.widgets.Graph;
-import org.eclipse.zest.core.widgets.GraphContainer;
-import org.eclipse.zest.core.widgets.GraphNode;
-import org.openarchitectureware.vis.zest.builder.GraphBuilder;
-import org.openarchitectureware.vis.zest.builder.GraphData;
 import org.openarchitectureware.vis.zest.builder.GraphMMModelWrapper;
 
 public class GraphContentProvider implements ITreeContentProvider {
@@ -21,75 +13,26 @@ public class GraphContentProvider implements ITreeContentProvider {
 		this.model = new GraphMMModelWrapper(graphmmModel);
 	}
 	public Object[] getChildren(Object parentElement) {
-//		if (parentElement instanceof Graph)
-//			return ((Graph)parentElement).getNodes().toArray();
-//		if (parentElement instanceof GraphNode)
-//		{
-//			EObject obj = builder.getCorrespondingModelObject((GraphNode)parentElement);
-//			EObject childGraph = builder.getChildGraph(obj);
-//			if (childGraph!=null)
-//			{
-//				List<GraphNode> nodes = new ArrayList<GraphNode>();
-//				for (EObject node : model.getNodes(childGraph))
-//				{
-//					nodes.add(builder.getCorrespondingGraphNode(node));
-//				}
-////				return nodes.toArray();
-//				return model.getNodes(childGraph).toArray();
-//			}
-//			else
-//			return new Object[]{};
-//		}
-//		if (parentElement instanceof GraphContainer)
-//			return ((GraphContainer)parentElement).getNodes().toArray();
-//		else return null;
-//		if (model.isContainerNode((EObject) parentElement))
-//			return new Object[]{model.getContainedGraph((EObject) parentElement)};
 		if (model.isGraphCollection((EObject) parentElement))
 			return model.getGraphs((EObject) parentElement).toArray();
 		if (model.isGraph((EObject) parentElement))
 			return model.getNodes((EObject) parentElement).toArray();
+		if (model.isContainerNode((EObject) parentElement))
+			return ((EObject)parentElement).eContents().get(0).eContents().toArray();
 		return ((EObject)parentElement).eContents().toArray();
 	}
 
 	public Object getParent(Object element) {
-//		if (element instanceof Graph)
-//		{
-//			EObject supergraph = builder.getContainingGraph(((GraphData)((Graph)element).getData()).getModelNode());
-//			return supergraph;
-//		}
-//		if (element instanceof GraphNode)
-//		{
-//			EObject obj = model.getContainingGraphForNode(builder.getCorrespondingModelObject((GraphNode)element));
-//			return obj;//builder.getContainingGraph(obj);
-//		}
-//		// TODO Auto-generated method stub
-//		return null;
-		return ((EObject)element).eContainer();
+		EObject container = ((EObject)element).eContainer();
+		if (container != null)
+		{  //hides the graph-node of ContainerNode
+			if (model.isGraph(container) && container.eContainer() != null && model.isContainerNode(container.eContainer()))
+					return container.eContainer();
+		}
+		return container;
 	}
 
 	public boolean hasChildren(Object element) {
-//		if (element instanceof Graph)
-//			return ((Graph)element).getNodes().size() > 0;
-//		if (element instanceof GraphNode)
-//		{
-//			EObject obj = builder.getCorrespondingModelObject((GraphNode)element);
-//			EObject childGraph = builder.getChildGraph(obj);
-//			if (childGraph!=null)
-//			{
-//				List<GraphNode> nodes = new ArrayList<GraphNode>();
-//				for (EObject node : model.getNodes(childGraph))
-//				{
-//					nodes.add(builder.getCorrespondingGraphNode(node));
-//				}
-//				return nodes.size()>0;
-//			}
-//			else
-//			return false;
-//		}
-//		if (element instanceof GraphContainer)
-//			return ((GraphContainer)element).getNodes().size()>0;
-//		else return false;
 		return getChildren(element).length>0;
 	}
 
