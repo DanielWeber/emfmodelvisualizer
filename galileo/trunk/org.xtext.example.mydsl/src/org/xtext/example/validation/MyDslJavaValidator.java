30b4957583c10c97c9889d6d91b6b360a9acea3a
Markus Voelter <voelter@no.mail>
2009-08-03 09:23:01 +0000

    git-svn-id: http://emfmodelvisualizer.googlecode.com/svn@195 d18ea948-b79e-11dd-b040-2d45f096adfe

---
+++ galileo/trunk/org.xtext.example.mydsl/src/org/xtext/example/validation/MyDslJavaValidator.java
@@ -0,0 +1,13 @@
+package org.xtext.example.validation;
+ 
+
+public class MyDslJavaValidator extends AbstractMyDslJavaValidator {
+
+//	@Check
+//	public void checkTypeNameStartsWithCapital(Type type) {
+//		if (!Character.isUpperCase(type.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
+//		}
+//	}
+
+}
