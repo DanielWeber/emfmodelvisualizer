6cf280d53943b24665209f50ac26eeeaf4acbd3f
Christoph Kulla <ChristophKulla@no.mail>
2009-09-10 17:24:38 +0000

    Initialize uid with 1 instead of 0, currentUid returns now the current id and not the next one.
    git-svn-id: http://emfmodelvisualizer.googlecode.com/svn@220 d18ea948-b79e-11dd-b040-2d45f096adfe

--- galileo/trunk/org.openarchitectureware.vis.graphviz/src/org/openarchitectureware/vis/graphviz/UidUtil.java
+++ galileo/trunk/org.openarchitectureware.vis.graphviz/src/org/openarchitectureware/vis/graphviz/UidUtil.java
@@ -2,13 +2,17 @@ package org.openarchitectureware.vis.graphviz;
 
 public class UidUtil {
 
-	private static int uid = 0;
+	private static int uid = 1;
 	
 	public static String newUid( Object o ) {
 		return new Integer(uid++).toString();
 	}
 	
+	public static String newUid( Object o, Object uniqueToken ) {
+		return new Integer(uid++).toString();
+	}
+	
 	public static String currentUid () {
-		return new Integer(uid).toString();
+		return new Integer(uid-1).toString();
 	}
 }
