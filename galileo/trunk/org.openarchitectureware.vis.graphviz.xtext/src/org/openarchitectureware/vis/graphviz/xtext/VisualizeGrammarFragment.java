376fc9344bdbf917067b068862290edfa4c04513
Christoph Kulla <ChristophKulla@no.mail>
2009-09-10 17:27:27 +0000

    Initial version of xtext grammar to syntax diagram transformation.
    git-svn-id: http://emfmodelvisualizer.googlecode.com/svn@223 d18ea948-b79e-11dd-b040-2d45f096adfe

---
+++ galileo/trunk/org.openarchitectureware.vis.graphviz.xtext/src/org/openarchitectureware/vis/graphviz/xtext/VisualizeGrammarFragment.java
@@ -0,0 +1,63 @@
+package org.openarchitectureware.vis.graphviz.xtext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.mwe.core.WorkflowFacade;
+import org.eclipse.xpand2.XpandExecutionContext;
+import org.eclipse.xtext.Grammar;
+import org.eclipse.xtext.GrammarUtil;
+import org.eclipse.xtext.generator.DefaultGeneratorFragment;
+import org.eclipse.xtext.generator.Generator;
+import org.eclipse.xtext.generator.Naming;
+
+/**
+ * A Xtext generator fragment to transform the grammar into a
+ * graphviz dot file and generate pictures by executing graphviz.
+ * 
+ * @author Christoph Kulla
+ */
+public class VisualizeGrammarFragment extends DefaultGeneratorFragment {
+	
+	public void generate(Grammar grammar, XpandExecutionContext ctx) {
+		super.generate (grammar, ctx);
+				
+		String targetDir = ctx.getOutput().getOutlet(Generator.SRC_GEN).getPath()+"/"+Naming.asPath(GrammarUtil.getNamespace(grammar));
+		
+		String wfFile = "org/openarchitectureware/vis/graphviz/xtext/visualizeGrammar.mwe";
+		Map properties = new HashMap();
+		properties.put ("targetDir", targetDir);
+		properties.put("showUsedGrammars", Boolean.toString(showUsedGrammars));
+		properties.put ("createBatchFile", "false");
+		properties.put ("executeGraphviz", "true");
+		properties.put("inputSlot", "model");
+		if (outputFormat!=null)
+			properties.put ("outputFormat", outputFormat);
+		Map slotContents = new HashMap();
+		slotContents.put("model", grammar);
+		WorkflowFacade wf = new WorkflowFacade (wfFile, properties);
+		wf.run(slotContents);
+	}
+	
+	private String outputFormat;
+
+	public String getOutputFormat() {
+		return outputFormat;
+	}
+
+	public void setOutputFormat(String outputFormat) {
+		this.outputFormat = outputFormat;
+	}
+	
+	public void setShowUsedGrammars(boolean showUsedGrammars) {
+		this.showUsedGrammars = showUsedGrammars;
+	}
+
+	public boolean isShowUsedGrammars() {
+		return showUsedGrammars;
+	}
+
+	private boolean showUsedGrammars = false;
+	
+	
+}
