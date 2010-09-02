2470cff01cac87630690c302317576265f318bff
Christoph Kulla <christophkulla@googlemail.com>
2010-09-02 19:47:24 +0200

    Added test for grammar metamodel visualization workflow.

---
+++ org.openarchitectureware.vis.test/src/org/openarchitectureware/vis/graphviz/xtext/VisualizeGrammarMetamodelTest.java
@@ -0,0 +1,28 @@
+package org.openarchitectureware.vis.graphviz.xtext;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.mwe2.language.Mwe2StandaloneSetup;
+import org.junit.Test;
+import org.openarchitectureware.vis.Mwe2Runner;
+
+import com.google.inject.Injector;
+
+public class VisualizeGrammarMetamodelTest {
+
+	@Test
+	public void ecoreEcoreVisualization() {
+		Injector injector = new Mwe2StandaloneSetup().createInjectorAndDoEMFRegistration();
+		Mwe2Runner mweRunner = injector.getInstance(Mwe2Runner.class);
+		Map<String, String> parameters = new HashMap<String, String>();	
+		parameters.put ("targetDir", "src-gen/VisualizeGrammarMetamodel/");
+		mweRunner.run("org.openarchitectureware.vis.graphviz.xtext.VisualizeGrammarMetamodel", parameters);
+		
+		assertTrue(new File("src-gen/VisualizeGrammarMetamodel/xtext.dot").exists());
+		assertTrue(new File("src-gen/VisualizeGrammarMetamodel/xtext.gif").exists());
+	}
+}
