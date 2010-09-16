3afe013a3e673be06af2ba040dcb604dcb3427d9
DanielWeber <daniel.weber.dev@googlemail.com>
2010-09-16 17:08:57 +0200

    Added font color + an option to create separate syntax diagrams per rule
     - Replaced rule start box with a simple point, the rule's name is already present in the subgraph's title
     - Added support for "per rule" graphs. If createSeparateRuleGraphs is set to true, the transformation creates
       a separate dot graph per Xtext rule resulting in 1 graphics file per rule. This should make the generated
       graphics suitable for inclusion in a document when the "global" graph becomes too large.
     - Adapted font color to create diagrams similar antlrworks' diagram style

--- org.openarchitectureware.vis.graphviz.xtext/src/org/openarchitectureware/vis/graphviz/xtext/VisualizeGrammarFragment.java
+++ org.openarchitectureware.vis.graphviz.xtext/src/org/openarchitectureware/vis/graphviz/xtext/VisualizeGrammarFragment.java
@@ -11,67 +11,89 @@ import org.eclipse.xtext.generator.DefaultGeneratorFragment;
 import org.eclipse.xtext.generator.Generator;
 import org.eclipse.xtext.generator.Naming;
 
-import com.google.inject.Inject;
 import com.google.inject.Injector;
 
 /**
- * A Xtext generator fragment to transform the grammar into a
- * graphviz dot file and generate pictures by executing graphviz.
- * 
+ * A Xtext generator fragment to transform the grammar into a graphviz dot file and
+ * generate pictures by executing graphviz.
+ *
  * @author Christoph Kulla
  */
-public class VisualizeGrammarFragment extends DefaultGeneratorFragment {
+public class VisualizeGrammarFragment extends DefaultGeneratorFragment
+{
 
-	Naming naming = new Naming();	
+   Naming          naming             = new Naming();
 
-	private String outputFormat;
-	
-	private boolean sortRulesByName = false;
-	
-	private boolean showUsedGrammars = false;
-	
-	public void generate(Grammar grammar, XpandExecutionContext ctx) {
+   private String  outputFormat;
 
-		super.generate (grammar, ctx);
-				
-		String targetDir = ctx.getOutput().getOutlet(Generator.SRC_GEN).getPath()+"/"+naming.asPath(GrammarUtil.getNamespace(grammar));
-		
-		Injector injector = new Mwe2StandaloneSetup().createInjectorAndDoEMFRegistration();
-		Mwe2Runner mweRunner = injector.getInstance(Mwe2Runner.class);
-		Map<String, String> parameters = new HashMap<String, String>();	
-		parameters.put ("targetDir", targetDir);
-		parameters.put ("showUsedGrammars", String.valueOf(showUsedGrammars));
-		parameters.put ("sortRulesByName", String.valueOf (sortRulesByName));
-		parameters.put("inputSlot", "model");
-		if (outputFormat!=null)
-			parameters.put ("outputFormat", outputFormat);
-		mweRunner.getWorkflowContext().put("model", grammar);
-		mweRunner.run("org.openarchitectureware.vis.graphviz.xtext.VisualizeGrammar", parameters);
-	}
+   private boolean sortRulesByName    = false;
 
+   private boolean showUsedGrammars   = false;
 
-	public String getOutputFormat() {
-		return outputFormat;
-	}
+   private boolean createSeparateRuleGraphs = false;
 
-	public void setOutputFormat(String outputFormat) {
-		this.outputFormat = outputFormat;
-	}
-	
-	public void setShowUsedGrammars(boolean showUsedGrammars) {
-		this.showUsedGrammars = showUsedGrammars;
-	}
+   private String imageSubdirectory = "";
 
-	public boolean isShowUsedGrammars() {
-		return showUsedGrammars;
-	}
+   public void generate(Grammar grammar, XpandExecutionContext ctx)
+   {
 
-	public void setSortRulesByName(boolean sortRulesByName) {
-		this.sortRulesByName = sortRulesByName;
-	}
+      super.generate(grammar, ctx);
 
-	public boolean isSortRulesByName() {
-		return sortRulesByName;
-	}
+      String targetDir = ctx.getOutput().getOutlet(Generator.SRC_GEN).getPath() + "/"
+            + naming.asPath(GrammarUtil.getNamespace(grammar))+ "/" + imageSubdirectory ;
 
+      Injector injector = new Mwe2StandaloneSetup().createInjectorAndDoEMFRegistration();
+      Mwe2Runner mweRunner = injector.getInstance(Mwe2Runner.class);
+      Map<String, String> parameters = new HashMap<String, String>();
+      parameters.put("targetDir", targetDir);
+      parameters.put("showUsedGrammars", String.valueOf(showUsedGrammars));
+      parameters.put("sortRulesByName", String.valueOf(sortRulesByName));
+      parameters.put("createSeparateRuleGraphs", String.valueOf(createSeparateRuleGraphs));
+      parameters.put("inputSlot", "model");
+      if(outputFormat != null)
+         parameters.put("outputFormat", outputFormat);
+      mweRunner.getWorkflowContext().put("model", grammar);
+      mweRunner.run("org.openarchitectureware.vis.graphviz.xtext.VisualizeGrammar",
+            parameters);
+   }
+
+   public String getOutputFormat()
+   {
+      return outputFormat;
+   }
+
+   public void setOutputFormat(String outputFormat)
+   {
+      this.outputFormat = outputFormat;
+   }
+
+   public void setShowUsedGrammars(boolean showUsedGrammars)
+   {
+      this.showUsedGrammars = showUsedGrammars;
+   }
+
+   public boolean isShowUsedGrammars()
+   {
+      return showUsedGrammars;
+   }
+
+   public void setSortRulesByName(boolean sortRulesByName)
+   {
+      this.sortRulesByName = sortRulesByName;
+   }
+
+   public boolean isSortRulesByName()
+   {
+      return sortRulesByName;
+   }
+
+   public void setCreateSeparateRuleGraphs(boolean createSeparateRuleGraphs)
+   {
+      this.createSeparateRuleGraphs = createSeparateRuleGraphs;
+   }
+
+   public void setImageSubdirectory(String imageSubdirectory)
+   {
+      this.imageSubdirectory = imageSubdirectory;
+   }
 }
